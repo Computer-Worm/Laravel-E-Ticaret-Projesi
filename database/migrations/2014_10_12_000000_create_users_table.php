36ea85ec6356ba7e77bcc770a7957d6d60b4f27d
Computer-Worm <yusufhasarr@gmail.com>
2023-03-31 23:29:16 +0300

    'modle' ve 'migrations' a gerekli alanlar eklendi

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,11 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->id(column:'user_id');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean(column:'is_admin');
+            $table->boolean(column:'is_active');
             $table->rememberToken();
             $table->timestamps();
         });
