48231058b4838698745ff47e2442da25d49656d1
Computer-Worm <yusufhasarr@gmail.com>
2023-04-15 21:28:18 +0300

    controller modeline bakıldı

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Bacend\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/user', [UserController::class, "index"]);
+
+Route::resource('/users', UserController::class);
